0783ca7bf5cec0d682fc7ab122847d1e72143ef8
Kyrie1965 <yuri_yuriev@mail.ru>
2019-01-24 00:41:00 +0300

    Add files via upload

--- 100-redirect.sh
+++ 100-redirect.sh
@@ -4,6 +4,7 @@
 [ "$table" != "nat" ] && exit 0
 
 if [ -z "$(iptables-save 2>/dev/null | grep unblock)" ]; then
+    ipset create unblock hash:net -exist
     iptables -w -t nat -A PREROUTING -i br0 -p tcp -m set --match-set unblock dst -j REDIRECT --to-port 9141
 fi
 
